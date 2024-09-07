c84d4a6365dbe20d9d7d06238d6386292f604bf2
Lim Jit <limjit30@gmail.com>
2024-09-07 12:41:01 +0800

    Neoscroll custom keymap function deprecated; updated to new function.

--- nvim/.config/nvim/after/plugin/neoscroll.rc.lua
+++ nvim/.config/nvim/after/plugin/neoscroll.rc.lua
@@ -1,8 +1,9 @@
-local neoscroll_avail, neoscroll_config = pcall(require, "neoscroll.config")
-if neoscroll_avail then
-  neoscroll_config.set_mappings {
-    -- Syntax: [keys] = {function, {function arguments}}
-        ["<C-u>"] = { "scroll", { "-0.4", "true", "200", [['quadratic']] } },
-        ["<C-d>"] = { "scroll", { "0.4", "true", "200", [['quadratic']] } },
+neoscroll = require('neoscroll')
+local keymap = {
+  ["<C-u>"] = function() neoscroll.scroll({ "-0.4", "true", "200", [['quadratic']] }) end;
+  ["<C-d>"] = function() neoscroll.scroll({ "0.4", "true", "200", [['quadratic']] }) end;
 }
+local modes = { 'n', 'v', 'x' }
+for key, func in pairs(keymap) do
+  vim.keymap.set(modes, key, func)
 end
